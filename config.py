bc9b8a62799e845b715dadbdb8ed2d14440cf555
grandrea <44289027+grandrea@users.noreply.github.com>
2022-11-29 12:40:50 +0100

    convert path in config.py to raw string

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-msconvert_exe = 'C:\Program Files\ProteoWizard\ProteoWizard 3.0.22298.76c82e9\msconvert.exe'
+msconvert_exe = r'C:\Users\my.name\AppData\Local\Apps\ProteoWizard 3.0.22314.0cd8422 64-bit\msconvert.exe'
 mscon_settings = ['peakPicking true 2-', 'msLevel 2-', 'titleMaker <RunId>.<ScanNumber>.<ScanNumber>.<ChargeState>']
 split_acq = False
 detector_filter = 'all'
